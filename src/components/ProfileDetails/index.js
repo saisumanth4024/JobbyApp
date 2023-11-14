d41d9aafba713fe747312a968059ee33c94ae3f0
sai <sai@gmail.com>
2023-11-14 13:19:57 +0000

    JobbyApp

--- src/components/ProfileDetails/index.js
+++ src/components/ProfileDetails/index.js
@@ -19,7 +19,7 @@ const ProfileDetails = props => {
     return (
       <div className="profile-details-container">
         <img src={profileImageUrl} alt="profile" className="profile-image" />
-        <h1 className="profile-name">Santosh Kumar</h1>
+        <h1 className="profile-name">A B V Sai Sumanth</h1>
         <p className="profile-bio">{shortBio}</p>
       </div>
     )
